d24d650450177eee93ecb852ec2cbb1f65032661
karanthorave45 <thoravekaran45@gmail.com>
2025-07-01 02:14:43 +0530

    Create AddRentaljsp.java

---
+++ AddRentaljsp.java
@@ -0,0 +1,11 @@
+<html>
+  <body>
+    <h2>Add Rental Entry</h2>
+    <form action="addRental" method="post">
+      From: <input type="text" name="from"><br>
+      To: <input type="text" name="to"><br>
+      Amount: <input type="text" name="amount"><br>
+      <input type="submit" value="Add Rental">
+    </form>
+  </body>
+</html>
